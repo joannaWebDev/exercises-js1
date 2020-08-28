fd786704342afbbfc6a642abb88e92175b880ccf
joannaWebDev <joannasmerea@gmail.com>
2020-08-28 16:09:18 +0200

    ok

--- week-3/Homework/E-array-map/exercise.js
+++ week-3/Homework/E-array-map/exercise.js
@@ -1,5 +1,15 @@
 // Using the .map() method, create a new array with `numbers` multiplied by 100
 // Write multiple solutions using different syntax (as shown in the README)
 
-var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+const numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+//ECMA 6
+let multipliedNum = numbers.map(number => number * 100);
+
+//OLD_WAYS
+// function multiply(number) {
+//     return number *100;
+// }
+// let multipliedNum = numbers.map(multiply)
+
+console.log(multipliedNum);
