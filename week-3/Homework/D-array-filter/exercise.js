151006484758c43d96f16cc3a540de0c7ba10cc1
joannaWebDev <joannasmerea@gmail.com>
2020-08-29 10:08:09 +0200

    02 exercises pending

--- week-3/Homework/D-array-filter/exercise.js
+++ week-3/Homework/D-array-filter/exercise.js
@@ -8,7 +8,10 @@
 
 var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-var pairsByIndex; // Complete this statement
+var pairsByIndex = pairsByIndexRaw.filter(function(x) {
+   if (x % 2 === 0);
+  return `bla`
+}); // Complete this statement
 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
